27cf7c449edbc83878d5dadd76adc926b38a1ec0
ranched <jasosmc@yahoo.com>
2017-12-06 14:29:34 -0600

    classes and hover finished

--- app.js
+++ app.js
@@ -1,16 +1,83 @@
 
 
+// var GroceryList = (props) => (
+// <ul>
+//   <GroceryListItem item={props.food[0]}/>
+//   <GroceryListItem item={props.food[1]}/>
+// </ul>
+// );
+
+// const liStyle = {};
+
+// class GroceryListItem extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//   onItemHover () {
+//     console.log('running')
+//     liStyle = {font-weight: bold}
+//   }
+  
+//   render () {
+//     return (
+//       <li onHover={onItemHover} /*style={liStyle}>*/ {this.props.item} </li>
+//     );
+//   }
+// }
+
+// ReactDOM.render(<GroceryList food={['apples', 'oranges']}/>, document.getElementById('app'));
+
+
+
+// CLASSES WORK
+// var GroceryList = (props) => (
+//   <ul>
+//     <GroceryListItem item={props.food[0]} />
+//     <GroceryListItem item={props.food[1]} />
+//   </ul>
+// );
+
+// class GroceryListItem extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//   render() {
+//     return(
+//       <li>{this.props.item}</li>  
+//     );
+//   }
+// }
+
+// ReactDOM.render(<GroceryList food={['apples', 'oranges']}/>, document.getElementById('app'));
+
+
+
+// TRYING TO GET CLICK HANDLER TO WORK
 var GroceryList = (props) => (
-<ul>
-  <GroceryListItem item={props[0]}/>
-  <GroceryListItem item={props[1]}/>
-</ul>
+  <ul>
+    <GroceryListItem item={props.food[0]} />
+    <GroceryListItem item={props.food[1]} />
+  </ul>
 );
 
-var GroceryListItem = (props) => (
-  <li> {props.item} </li>
-);
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.liStyle = {};
+    this.state = {clicked: false};
+  }
+  
+  clickHandler() {
+    this.setState({clicked: !this.state.clicked});
+  }
+  
+  render() {
+    let style = {fontWeight: this.state.clicked ? 'bold' : 'normal'};
+    return(
+      <li onClick={this.clickHandler.bind(this)} style={style}>{this.props.item}</li>  
+    );
+  }
+}
 
-ReactDOM.render(<GroceryList {['apples', 'oranges']}/>, document.getElementById('app'));
-
+ReactDOM.render(<GroceryList food={['apples', 'oranges']}/>, document.getElementById('app'));
 
